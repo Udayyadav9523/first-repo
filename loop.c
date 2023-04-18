e46ddd3949bf7949eec241c317f807b72a1bcd56
Uday kumar <udayyadav9523@gmail.com>
2023-04-18 18:44:22 +0530

    first commit

---
+++ loop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("The number rows and columms:");
+    scanf("%d", &n);
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i + j >= n - 1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
